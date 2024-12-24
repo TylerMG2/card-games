52fb93973006fc4f65b9801adba007acc45f33a6
Tyler Gwin <s3946514@student.rmit.edu.au>
2024-12-25 01:34:44 +1100

    Implemented sending functions for ServerRoom

--- shared/src/traits.rs
+++ shared/src/traits.rs
@@ -1,8 +1,13 @@
-use crate::{events, types};
+use serde::{de::DeserializeOwned, Serialize};
 
-pub trait GameLogic {
-    type GameServerEvent;
-    type GameClientEvent;
+use crate::types;
+
+pub trait GameLogic 
+where 
+    Self: Sized
+{
+    type GameServerEvent: DeserializeOwned + Serialize;
+    type GameClientEvent: DeserializeOwned + Serialize;
     type Room: Default + Copy;
     type Player: Player + Default + Copy;
 
@@ -10,8 +15,10 @@ pub trait GameLogic {
 
     // The server should not update the room directly, instead it should send server events to the clients which will update their rooms and the
     // server room using the handle_server_event method
-    fn handle_client_event(&self, event: &Self::GameClientEvent, room: &mut types::ServerRoom<Self::Room, Self::Player>, player_index: usize);
-    fn handle_server_event(&self, event: &Self::GameServerEvent, room: &mut Self::Room, players: &mut [Option<Self::Player>; 8]);
+    fn handle_client_event(&self, event: &Self::GameClientEvent, room: &mut types::ServerRoom<Self>, player_index: usize);
+
+    // Player index should be provided all the time from the client, for the server its only provided when the server is sending an event to a specific player
+    fn handle_server_event(&self, event: &Self::GameServerEvent, room: &mut Self::Room, players: &mut [Option<Self::Player>; 8], player_index: Option<usize>);
 }
 
 pub trait Player {
--- shared/src/types.rs
+++ shared/src/types.rs
@@ -1,10 +1,11 @@
 use tokio::sync::mpsc::UnboundedSender;
 
-use crate::traits::Player;
+use crate::{events::{ServerEvent, ToFromBytes}, traits::{GameLogic, Player}};
 
 pub enum GameType {
     Tycoon,
     Carbo,
+    Default,
 }
 
 pub struct ClientRoom<RoomType: Default + Copy, PlayerType: Player + Default + Copy> {
@@ -14,12 +15,77 @@ pub struct ClientRoom<RoomType: Default + Copy, PlayerType: Player + Default + C
     pub room: RoomType,
 }
 
-pub struct ServerRoom<RoomType: Default + Copy, PlayerType: Player + Default + Copy> {
+pub struct ServerRoom<Logic: GameLogic> {
     pub connections: [Option<Connection>; 8], // TODO: Move to a constant
-    pub client_room: ClientRoom<RoomType, PlayerType>,
+    pub client_room: ClientRoom<Logic::Room, Logic::Player>,
+    pub logic: Logic,
 }
 
 pub struct Connection {
     pub id: uuid::Uuid,
-    pub sender: UnboundedSender<Vec<u8>>,
+    pub sender: Option<UnboundedSender<Vec<u8>>>,
+}
+
+impl<Logic> ServerRoom<Logic>
+where
+    Logic: GameLogic,
+{
+    pub fn new(logic: Logic) -> Self {
+        Self {
+            connections: [const { None }; 8],
+            client_room: ClientRoom {
+                players: [None; 8],
+                host: 0,
+                game: GameType::Default,
+                room: Logic::Room::default(),
+            },
+            logic,
+        }
+    }
+
+    pub fn send_to_all(&mut self, event: &ServerEvent<Logic::GameServerEvent>) {
+        handle_server_event(self, event, None);
+
+        for connection in self.connections.iter() {
+            if let Some(connection) = connection {
+                if let Some(sender) = &connection.sender {
+                    sender.send(event.to_bytes()).unwrap(); // TODO: Handle send error
+                }
+            }
+        }
+    }
+
+    pub fn send_except(&mut self, event: &ServerEvent<Logic::GameServerEvent>, except: usize) {
+        handle_server_event(self, event, None);
+
+        for (index, connection) in self.connections.iter().enumerate() {
+            if index != except {
+                if let Some(connection) = connection {
+                    if let Some(sender) = &connection.sender {
+                        sender.send(event.to_bytes()).unwrap();
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn send_to(&mut self, event: &ServerEvent<Logic::GameServerEvent>, to: usize) {
+        handle_server_event(self, event, Some(to));
+
+        if let Some(connection) = self.connections[to].as_ref() {
+            if let Some(sender) = &connection.sender {
+                sender.send(event.to_bytes()).unwrap();
+            }
+        }
+    }
+}
+
+fn handle_server_event<Logic: GameLogic>(room: &mut ServerRoom<Logic>, event: &ServerEvent<Logic::GameServerEvent>, player_index: Option<usize>) {
+    match event {
+        ServerEvent::GameEvent(event) => {
+            room.logic.handle_server_event(&event, &mut room.client_room.room, &mut room.client_room.players, player_index);
+        },
+        ServerEvent::Unknown => panic!("Should never send a ServerEvent::Unknown"), 
+        _ => {}, // TODO: Implement normal event handling
+    }
 }
